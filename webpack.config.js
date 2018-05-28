7d9dc54598ffe0b496e5ceb9898bddc0cc86463a
fuzong.wang <fuzong.wang@ikang.com>
2018-05-28 18:55:24 +0800

    develop

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: ' Output Management'
-    })
+    }),
+    new ManifestPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   mode: 'production'
 };
